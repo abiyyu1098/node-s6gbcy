4a6440a788d3034daa0a1eb43cc5281382a984d6
abiyyu1098 <81797574+abiyyu1098@users.noreply.github.com>
2023-03-23 20:18:00 +0700

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -2,4 +2,14 @@
 
 console.log(`Hello Node.js v${process.versions.node}!`);
 
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send('work');
+});
+
+app.listen(3000, () => {
+  console.log("Fubuki's Working!!..");
+});
+const child_process = require('child_process');
